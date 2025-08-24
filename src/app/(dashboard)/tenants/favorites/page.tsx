2af4edd8e9b85e151c22487af56bf76e064be2be
Leyla-la <tranthigiangpltn@gmail.com>
2025-08-24 23:25:41 +0700

    favorites/residences page of tenants

--- src/app/(dashboard)/tenants/favorites/page.tsx
+++ src/app/(dashboard)/tenants/favorites/page.tsx
@@ -1,11 +1,65 @@
 "use client";
 
-import React from 'react'
+import Header from "@/components/header";
+import Loading from "@/components/loading";
+import Card from "@/components/card";
+import {
+  useGetAuthUserQuery,
+  useGetPropertiesQuery,
+  useGetTenantQuery,
+} from "@/state/api";
+import React from "react";
+import { Property } from "@/types/prismaTypes";
 
 const Favorites = () => {
+  const { data: authUser } = useGetAuthUserQuery();
+  const { data: tenant } = useGetTenantQuery(
+    authUser?.cognitoInfo?.userId || "",
+    {
+      skip: !authUser?.cognitoInfo?.userId,
+    }
+  );
+
+  const {
+    data: favoriteProperties,
+    isLoading,
+    error,
+  } = useGetPropertiesQuery(
+    {
+      favoriteIds: tenant?.favorites?.map((fav: { id: number }) => fav.id),
+    },
+    {
+      skip: !tenant?.favorites || tenant.favorites.length === 0,
+    }
+  );
+
+  if (isLoading) return <Loading />;
+  if (error) return <div>Error loading favorite properties.</div>;
+
   return (
-    <div>Favorites</div>
-  )
-}
+    <div className="dashboard-container">
+      <Header
+        title="Favorite Properties"
+        subtitle="List of properties you have marked as favorite."
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {favoriteProperties?.map((property) => (
+          <Card
+            key={property.id}
+            property={property}
+            isFavorite={true}
+            onFavoriteToggle={() => {}}
+            showFavoriteButton={false}
+            propertyLink={`tenants/residences/${property.id}`}
+          />
+        ))}
+      </div>
+
+      {(!favoriteProperties || favoriteProperties.length === 0) && (
+        <p>You don&lsquo;t have any favorite properties.</p>
+      )}
+    </div>
+  );
+};
 
 export default Favorites;
---
+++ src/app/(dashboard)/tenants/residences/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import Header from "@/components/header";
+import Loading from "@/components/loading";
+import Card from "@/components/card";
+import {
+  useGetAuthUserQuery,
+  useGetCurrentResidencesQuery,
+  useGetTenantQuery,
+} from "@/state/api";
+import React from "react";
+
+const Residences = () => {
+  const { data: authUser } = useGetAuthUserQuery();
+  const { data: tenant } = useGetTenantQuery(
+    authUser?.cognitoInfo?.userId || "",
+    {
+      skip: !authUser?.cognitoInfo?.userId,
+    }
+  );
+
+  const {
+    data: currentResidences,
+    isLoading,
+    error,
+  } = useGetCurrentResidencesQuery(
+    authUser?.cognitoInfo?.userId || "",
+    {
+      skip: !authUser?.cognitoInfo?.userId,
+    }
+  );
+
+  if (isLoading) return <Loading />;
+  if (error) return <div>Error loading current residences.</div>;
+
+  return (
+    <div className="dashboard-container">
+      <Header
+        title="Current Residences"
+        subtitle="List of properties you are currently residing in."
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {currentResidences?.map((property) => (
+          <Card
+          key={property.id}
+          property={property}
+          isFavorite={tenant?.favorites.includes(property.id) || false}
+          onFavoriteToggle={() => {}}
+          showFavoriteButton={false}
+          propertyLink={`tenants/residences/${property.id}`}
+        />
+        ))}
+      </div>
+
+      {(!currentResidences || currentResidences.length === 0) && (
+        <p>You don't have any current residences.</p>
+      )}
+    </div>
+  );
+};
+
+export default Residences;
